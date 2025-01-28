74479187885075444e489619c4e4add07afec429
Vahalcadi <davidepetrone04@gmail.com>
2025-01-28 17:01:56 +0100

    Finalised jump

--- Assets/Scripts/GyroscopicMovement.cs
+++ Assets/Scripts/GyroscopicMovement.cs
@@ -16,6 +16,8 @@ public class GyroscopicMovement : MonoBehaviour
     private float sqrShakeDetectionThreshold;
     private float timeSinceLastShake;
 
+    private bool IsGrounded;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -32,25 +34,41 @@ public class GyroscopicMovement : MonoBehaviour
         sqrShakeDetectionThreshold = Mathf.Pow(shakeDetectionThreshold, 2);
         moveVector = new Vector3(InputManager.Instance.Move().x, 0, -InputManager.Instance.Move().z);
 
-        if (InputManager.Instance.Move().magnitude >= sqrShakeDetectionThreshold
-            && Time.unscaledTime >= timeSinceLastShake + minShakeInterval)
+        if (IsGrounded)
         {
-            Jump(); //???
-            timeSinceLastShake = Time.unscaledTime;
-        }
+            if (InputManager.Instance.Jump().sqrMagnitude >= sqrShakeDetectionThreshold
+            && Time.unscaledTime >= timeSinceLastShake + minShakeInterval)
+            {
+                Jump();
+                timeSinceLastShake = Time.unscaledTime;
+            }
+        }   
 
-        Debug.Log(moveVector);
         //rb.linearVelocity = moveVector * speed;
     }
 
     private void FixedUpdate()
     {
-
         rb.AddForce(moveVector * acceleration, ForceMode.Force);
     }
 
     private void Jump()
     {
-        rb.AddForce(Vector3.up * yVelocity, ForceMode.Impulse);
+        //rb.AddForce(Vector3.up * yVelocity, ForceMode.Impulse);
+        rb.linearVelocity = Vector3.up * yVelocity;
+        IsGrounded = false;
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+            IsGrounded = true;
+    }
+
+    /*private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.collider.CompareTag("Ground"))
+            IsGrounded = false;
+    }*/
 }
