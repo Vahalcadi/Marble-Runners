a872361fd11e707b84e894b9c7fed3273b9a2421
Vahalcadi <davidepetrone04@gmail.com>
2025-02-14 00:30:28 +0100

    Two-way tube test

--- Assets/Scripts/Tube.cs
+++ Assets/Scripts/Tube.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -8,11 +9,16 @@ public class Tube : MonoBehaviour
     private Rigidbody ballRigidBody;
     private SplineContainer splineContainer;
 
-    [SerializeField] private SplineAnimate.Method animationMethod = SplineAnimate.Method.Speed;
-    [Header("if animationMethod is time")]
+    private Spline spline;
+
     [SerializeField] private float duration = 3;
-    [Header("if animationMethod is speed")]
-    [SerializeField] private float speed = 15;
+
+    [Header("check the boolean only if the tube is a two-way tube")]
+    [SerializeField] private bool isTwoWay = false;
+    [SerializeField] private int startingSpline; //the index of the first-way spline to use
+    [SerializeField] private int endingSpline; //the index of the last-way spline to use
+
+    private bool alternate;
 
     private bool hasStarted;
 
@@ -36,19 +42,53 @@ public class Tube : MonoBehaviour
             ballRigidBody.linearVelocity = Vector3.zero;
             ballRigidBody.angularVelocity = Vector3.zero;
 
-            ballSplineAnimate.AnimationMethod = animationMethod;
-
-            if (ballSplineAnimate.AnimationMethod == SplineAnimate.Method.Time)
-                ballSplineAnimate.Duration = duration;
-            else
-                ballSplineAnimate.MaxSpeed = speed;
+            ballSplineAnimate.AnimationMethod = SplineAnimate.Method.Time;
+            ballSplineAnimate.Duration = duration;
 
             ballSplineAnimate.ElapsedTime = 0;
             ballRigidBody.interpolation = RigidbodyInterpolation.None;
-            ballSplineAnimate.Completed += ResetCoroutine;
 
-            ballSplineAnimate.Play();
+            ballSplineAnimate.Completed += ResetCoroutine;
+            if (!isTwoWay)
+                ballSplineAnimate.Play();        
+            else
+                StartCoroutine(TwoWayCoroutine());
         }
+    }
+
+    private void AlternateSplines()
+    {
+        if (alternate)
+        {
+            spline = splineContainer.Splines[endingSpline];
+            alternate = false;
+        }
+        else
+        {
+            spline = splineContainer.Splines[startingSpline];
+            alternate = true;
+        }
+        
+    }
+
+    private IEnumerator TwoWayCoroutine()
+    {
+        AlternateSplines();
+        float t = 0;
+        float percentage = 0;
+
+        while (percentage <= 1)
+        {
+            yield return null;
+            t += Time.deltaTime;
+            percentage = t / duration;
+
+            Vector3 posEvaluated = spline.EvaluatePosition(percentage);
+
+            ballRigidBody.transform.position = new Vector3(transform.position.x + posEvaluated.x, transform.position.y + posEvaluated.y, transform.position.z + posEvaluated.z); 
+        }
+
+        ResetCoroutine();
     }
 
     private void ResetCoroutine()
