68a17460167a2b847dc5d7f650a05285174453b1
Davide Petrone <davidepetrone04@gmail.com>
2025-02-13 12:28:59 +0100

    Added stuff to main menu ui script

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private GameObject MainTitle;
+    [SerializeField] private GameObject WorldSelection;
+    [SerializeField] private GameObject[] LevelSelections;
+    [SerializeField] private GameObject Shop;
+    [SerializeField] private GameObject Settings;
+
+    public void OpenWorldSelection()
+    {
+        MainTitle.SetActive(false);
+        WorldSelection.SetActive(true);
+        Shop.SetActive(false);
+        Settings.SetActive(false);
+        foreach (GameObject go in LevelSelections)
+            go.SetActive(false);
+    }
+
+    public void OpenLevelSelection(int world)
+    {
+        MainTitle.SetActive(false);
+        WorldSelection.SetActive(false);
+        Shop.SetActive(false);
+        Settings.SetActive(false);
+        foreach (GameObject go in LevelSelections)
+            go.SetActive(false);
+
+        LevelSelections[world].SetActive(true);
+    }
+
+    public void OpenShop()
+    {
+        MainTitle.SetActive(false);
+        WorldSelection.SetActive(false);
+        Shop.SetActive(true);
+        Settings.SetActive(false);
+        foreach (GameObject go in LevelSelections)
+            go.SetActive(false);
+    }
+
+    public void OpenSetting()
+    {
+        MainTitle.SetActive(false);
+        WorldSelection.SetActive(false);
+        Shop.SetActive(false);
+        Settings.SetActive(true);
+        foreach (GameObject go in LevelSelections)
+            go.SetActive(false);
+    }
+}
