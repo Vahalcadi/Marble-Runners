800f83b0af5116c263da5c2c1665f71cfb8d93a9
Roberto Guerricchio <robyguerricchio@gmail.com>
2025-02-13 23:28:02 +0100

    added goooooal + coin

--- Assets/Guerricchio's Assets/Guerricchio's Script/Coin/Coin.cs
+++ Assets/Guerricchio's Assets/Guerricchio's Script/Coin/Coin.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 100f;
+    private static readonly Vector3 rotationAxis = new Vector3(1, 1, 0).normalized;
+
+
+    void Update()
+    {
+        transform.Rotate(rotationAxis, rotationSpeed * Time.deltaTime, Space.Self);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
