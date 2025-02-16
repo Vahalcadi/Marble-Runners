bfa914bec62ed8b75bf567a230fb7d73e5386448
Vahalcadi <davidepetrone04@gmail.com>
2025-02-16 22:49:37 +0100

    Implemented Blob shadow

--- Assets/Scripts/BlobShadow.cs
+++ Assets/Scripts/BlobShadow.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class BlobShadow : MonoBehaviour
 {
     Transform _transform;
     private Vector3 offset;
     [SerializeField] private Transform playerTransform;
+    private DecalProjector projector;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
