28e994bf01362541b59fd6bf3cecac4a33a02ad1
Roberto Guerricchio <robyguerricchio@gmail.com>
2025-02-21 00:44:46 +0100

    Spero sia l'ultimo

--- Assets/Guerricchio's Assets/Guerricchio's Script/Coin/MenuCoin.cs
+++ Assets/Guerricchio's Assets/Guerricchio's Script/Coin/MenuCoin.cs
@@ -12,10 +12,8 @@ public class MenuCoin : MonoBehaviour
 
     private void LoadCoinCount()
     {
-        // Carica il numero totale di monete raccolte da PlayerPrefs
         int totalCollectedCoins = 0;
 
-        // Supponiamo che tu abbia 2 livelli, puoi adattare questo ciclo in base al numero di livelli
         for (int levelIndex = 1; levelIndex <= 2; levelIndex++)
         {
             totalCollectedCoins += PlayerPrefs.GetInt("CollectedCoins_Level" + levelIndex, 0);
